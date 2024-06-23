11d8655fb27566313503b6af2c12c78029620fd3
gouthampai05 <gouthampai05@gmail.com>
2024-06-23 13:44:39 +0530

    Working

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const bodyParser = require("body-parser");
+
+//POST body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
+});
+
+const urlDictionary = {};
+
+//First key for the object item, will keep incrementing later
+let count = 1;
+
+const dns = require("dns");
+
+app.post("/api/shorturl", async (req, res) => {
+  let url = req.body.url;
+
+  // Prepend https:// if the URL doesn't start with http:// or https://
+  if (!/^https?:\/\//i.test(url)) {
+    url = "https://" + url;
+  }
+
+  try {
+    const hostname = new URL(url).hostname;
+
+    dns.lookup(hostname, (error, address) => {
+      if (error) {
+        res.json({ error: "invalid url" });
+      } else {
+        urlDictionary[count] = url;
+        count += 1;
+
+        res.json({ original_url: url, short_url: count - 1 });
+      }
+    });
+  } catch (error) {
+    // This will catch any errors from the URL constructor
+    res.json({ error: "invalid url" });
+  }
+});
+
+app.get("/api/shorturl/:urlid", (req, res) => {
+  try {
+    const urlid = req.params.urlid;
+
+    res.redirect(urlDictionary[urlid]);
+  } catch (error) {
+    throw error;
+  }
 });
